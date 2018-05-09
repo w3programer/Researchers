6e4c9f3484dc5dcb4edaebe73488adee40fc92e0
hesham tatawy <iheshamtatawy@icloud.com>
2018-05-09 13:50:53 +0300

    Initial Commit

---
+++ Researchers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Researchers
+//
+//  Created by hesham tatawy on 23/08/1439 AH.
+//  Copyright © 1439 alatheertech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
